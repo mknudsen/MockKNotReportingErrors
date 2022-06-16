b74c05a31e4d130cb99105e492164b4a12f4b89b
Martin Knudsen <martin.knudsen@otto.de>
2022-06-16 17:48:05 +0200

    add examples for isMocKKMock() behaviour for actual non-mocks

--- app/src/androidTest/java/com/example/mockknotreportingerrors/MockKTests.kt
+++ app/src/androidTest/java/com/example/mockknotreportingerrors/MockKTests.kt
@@ -4,6 +4,8 @@ import io.mockk.every
 import io.mockk.isMockKMock
 import io.mockk.mockk
 import io.mockk.verify
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
 import org.junit.Test
 
 class MockKTests {
@@ -17,6 +19,10 @@ class MockKTests {
         }
 
         val instanceOfConcreteClass: CannotMockedClosedImplExample = mockk()
+
+        assertFalse(isMockKMock(Object()))
+        assertFalse(isMockKMock("foo"))
+        assertFalse(isMockKMock(CannotMockedClosedImplExample()))
 
         val isMock = isMockKMock(instanceOfConcreteClass)
 
